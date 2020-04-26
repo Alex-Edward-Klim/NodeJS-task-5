46df8a57a938c2302eb54bee4bc0d19cf20902c9
Alex-Edward-Klim <alexklim3221128@gmail.com>
2020-04-26 19:12:21 +0300

    feat: implement hashing user password via bcrypt

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const usersRepo = require('./user.memory.repository');
 
 const User = require('./user.model');
@@ -8,8 +10,10 @@ const getUserById = id => usersRepo.getUserById(id);
 
 const createNewUser = async newUser => {
   const user = new User(newUser);
-  return usersRepo.createNewUser(User.toSend(user)).then(() => {
-    return User.toResponse(user);
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  const hashedUser = Object.assign(user, { password: hashedPassword });
+  return usersRepo.createNewUser(User.toSend(hashedUser)).then(() => {
+    return User.toResponse(hashedUser);
   });
 };
 
